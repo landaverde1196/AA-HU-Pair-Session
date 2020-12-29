6503e7753743784177c0d57c5118c647cea63e40
Alejandro Landaverde <landaverde1196@gmail.com>
2020-12-29 01:43:55 +0000

    ready for pair session

---
+++ src/sum.js
@@ -0,0 +1,15 @@
+// console.log('Hello AA')
+
+// const A = [1, 2, 3, 4, 5];
+
+// const B = [6, 7, 8, 9, 10];
+
+
+// console.log(A[3]);
+
+
+const sum = (a, b) => {
+    return a + b;
+}
+
+module.exports = sum;
