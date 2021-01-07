0ba3b4125c8163059610b969ce74dd9666c67e14
Alejandro Landaverde <landaverde1196@gmail.com>
2021-01-07 02:29:51 +0000

    failing test added

--- test/sum.test.js
+++ test/sum.test.js
@@ -3,3 +3,7 @@ const sum = require("../src/sum")
 test('adds 2 + 2 to equal 4', () => {
     expect(sum(2, 2)).toBe(4);
 });
+
+test('adds 2 + 2 to equal 6', () => {
+    expect(sum(2, 2)).toBe(6);
+});
